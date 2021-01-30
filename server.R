cd3ad9fef12e970bd6da7cafe1e40b9c4fa310d6
Eugene Ng <egn202@gmail.com>
2021-01-30 18:27:00 -0500

    observe works

--- server.R
+++ server.R
@@ -26,7 +26,7 @@ shinyServer(function(input, output, session) {
   })
   #quality ratings chart
   output$plot2 <- renderPlot({
-    quality = nydoe %>% filter(DBN == "01M020", Year == 2019) %>% pivot_longer(c(27:32), names_to = "Quality", values_to = "QScore") %>% select(Quality, QScore)
+    quality = nydoe %>% filter(Name == input$Name, Year == 2019) %>% pivot_longer(c(27:32), names_to = "Quality", values_to = "QScore") %>% select(Quality, QScore)
     quality$Quality = c("Rigorous Instruction","Collaborative Teachers","Supportive Enviornment","Leadership","Family-Community Ties","Trust")  
     
     ggplot(data=quality, aes(Quality, QScore)) + geom_bar(aes(fill = Quality), stat='identity',) + coord_flip() + labs(y = "Rating",x="") + theme_classic() + theme(legend.position ="none") + scale_fill_brewer(palette="Set1")
@@ -34,8 +34,8 @@ shinyServer(function(input, output, session) {
   })
   #ELA standards chart
   output$plot3 <- renderPlot({
-    school = nydoe %>% filter(DBN == "01M020") %>% select(Year, ELA.Std)
-    district = nydoe %>% filter(District=="1") %>% select(Year,ELA.Std) %>% group_by(Year) %>% summarise(ELA.Std = mean(ELA.Std, na.rm=T))
+    school = nydoe %>% filter(Name == input$Name) %>% select(Year, ELA.Std)
+    district = nydoe %>% filter(District==input$District) %>% select(Year,ELA.Std) %>% group_by(Year) %>% summarise(ELA.Std = mean(ELA.Std, na.rm=T))
     city = city = nydoe %>% group_by(Year) %>% summarise(ELA.Std = mean(ELA.Std, na.rm=T))
     graph=cbind(school, district$ELA.Std,city$ELA.Std)
     colnames(graph)[2:4] = c("School","District","City")
@@ -48,8 +48,8 @@ shinyServer(function(input, output, session) {
   })
   #Math Standards Chart
   output$plot4 <- renderPlot({
-    school = nydoe %>% filter(DBN == "01M020") %>% select(Year, Math.Std)
-    district = nydoe %>% filter(District=="1") %>% select(Year,Math.Std) %>% group_by(Year) %>% summarise(Math.Std = mean(Math.Std, na.rm=T))
+    school = nydoe %>% filter(Name == input$Name) %>% select(Year, Math.Std)
+    district = nydoe %>% filter(District==input$District) %>% select(Year,Math.Std) %>% group_by(Year) %>% summarise(Math.Std = mean(Math.Std, na.rm=T))
     city = city = nydoe %>% group_by(Year) %>% summarise(Math.Std = mean(Math.Std, na.rm=T))
     graph=cbind(school, district$Math.Std,city$Math.Std)
     colnames(graph)[2:4] = c("School","District","City")
@@ -63,13 +63,13 @@ shinyServer(function(input, output, session) {
   
   #School demographics
   output$plot5 <- renderPlot({
-    nydoe %>% filter(DBN == "01M020") %>% select(Year, Asian, Black, Hisp, White) %>% pivot_longer(c(2:5), names_to = "Race", values_to = "Pct") %>%
+    nydoe %>% filter(Name == input$Name) %>% select(Year, Asian, Black, Hisp, White) %>% pivot_longer(c(2:5), names_to = "Race", values_to = "Pct") %>%
     ggplot()+geom_bar(aes(x=Year, y=Pct, fill=Race),stat="identity") + scale_fill_brewer(palette="Set1")+theme_classic()      
   })
   
   # show school info DataTable
   output$table1 <- DT::renderDataTable({
-    school_info = nydoe %>% filter(DBN == "01M020") %>% select(Year, Enrollment,Principal,Teachers.3) %>%
+    school_info = nydoe %>% filter(Name == input$Name) %>% select(Year, Enrollment,Principal,Teachers.3) %>%
       pivot_longer(c(2:4), names_to = "Info", values_to = "Pct") %>% pivot_wider(names_from =
                                                                                    Year, values_from = Pct)
     school_info$Info=c("Enrollment","Principal Tenure","Teacher with +3Y Exp %")
@@ -78,7 +78,7 @@ shinyServer(function(input, output, session) {
   
   # graph addtl details
   output$plot6 <- renderPlot({
-    g = nydoe %>% filter(DBN == "01M020") %>% select(Year, Absent,ELL.,Diab.)
+    g = nydoe %>% filter(Name == input$Name) %>% select(Year, Absent,ELL.,Diab.)
     colnames(g)[2:4] = c("Chronically Absent","English Lang.Learners","Special Needs")
     g = pivot_longer(g,c(2:4), names_to="Type", values_to = "Pct")
     
@@ -90,7 +90,7 @@ shinyServer(function(input, output, session) {
   
   # attendance graph
   output$plot7 <- renderPlot({
-    school_info = nydoe %>% filter(DBN == "01M020") %>% select(Year, Attendance,Teacher.Attendance)
+    school_info = nydoe %>% filter(Name == input$Name) %>% select(Year, Attendance,Teacher.Attendance)
     colnames(school_info)[2:3] = c("Attendance ","Teacher Attendance ")
     school_info = pivot_longer(school_info,c(2:3), names_to="Type", values_to = "Pct")
     
